3a98ee7b0e73dfe10b93a5ff02b1b8ab18103b49
Noor <noorani.bakerally@gmail.com>
2013-03-19 17:16:48 +0000

    Statistics Working

--- dispatcher.py
+++ dispatcher.py
@@ -7,9 +7,24 @@ app = Flask(__name__)
 def showAuthors():
     return render_template('authors.html',authors=data.authors)
 
-@app.route("/publications")
+@app.route("/publications",methods=['GET', 'POST'])
 def showPublications():
     return render_template('publications.html',publications = data.publications)
+
+
+@app.route("/publications/search",methods=['GET', 'POST'])
+def searchPublications():
+    results = {}
+    print request.form
+    return render_template('publications.html',publications = data.publications)
+	
+
+@app.route("/statistics")
+def showStatistics():
+    pub_types = {"conference_paper": 0, "book_chapter": 0,"book": 0,"journal":0}
+    for publication in data.publications:
+	pub_types[data.publications[publication].pub_type] = pub_types[data.publications[publication].pub_type] + 1 
+    return render_template('statistics.html',publications = data.publications,authors = data.authors,pub_types=pub_types)
 
 if __name__ == "__main__":
     app.run(debug=True)
